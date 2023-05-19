925e81f5d0b044a76a3bf3554dcf2f34e17293a8
Peter Wawa <meelisn@outlook.com>
2023-05-19 17:42:21 +0300

    update color change
    don't try to change ISE colors

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,6 +1,8 @@
 ﻿#region Verbose and Debug colors back to Cyan
-$Host.PrivateData.VerboseForegroundColor = [System.ConsoleColor]::Cyan
-$Host.PrivateData.DebugForegroundColor = [System.ConsoleColor]::Cyan
+if ($Host.Name -like 'ConsoleHost') {
+    $Host.PrivateData.VerboseForegroundColor = [System.ConsoleColor]::Cyan
+    $Host.PrivateData.DebugForegroundColor = [System.ConsoleColor]::Cyan
+}
 
 if ($PSVersionTable.PSVersion -gt '7.2') {
     $PSStyle.Formatting.Verbose = $PSStyle.Foreground.Cyan
