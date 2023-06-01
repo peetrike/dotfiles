e9d44e3ecc58d64c4ef71505973a5e947ec912f6
Peter Wawa <meelisn@outlook.com>
2023-06-01 17:47:05 +0300

    Update TerminalBlocks prompt sample

--- PowerShell/TerminalBlocks/Set-Prompt.ps1
+++ PowerShell/TerminalBlocks/Set-Prompt.ps1
@@ -1,43 +1,41 @@
 ﻿#Requires -Modules TerminalBlocks
 
-if (-not (Get-Command Test-IsAdmin -ErrorAction SilentlyContinue)) {
-    function Test-IsAdmin {
-        [CmdletBinding()]
-        param()
-
-        $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
-        ([Security.Principal.WindowsPrincipal] $currentUser).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-    }
-}
-
 $PromptSymbol = '❯ '
 
 $global:Prompt = @(
-    $caps = @{
-        #Caps = '▐', ''
-        Caps = '', ''
+    Initialize-Prompt
+
+    $Begincaps = @{
+        Caps = '▐', ''
     }
-    Show-ElapsedTime -Autoformat -Foreground White -Prefix '' #-BackgroundColor DarkBlue @caps
+    $Caps = @{
+        Caps = '', ''
+    }
+    Show-ElapsedTime -Autoformat <# -Foreground White #> -Prefix '' #-BackgroundColor DarkBlue @Begincaps
+
+    Show-Path -DriveName -HomeString "&House;" -Foreground White -AsUrl -BackgroundColor Blue @Begincaps
         # -Separator '' # This separator requires a nerdfont
-    Show-Path -DriveName -HomeString "&House;" -Foreground 'SteelBlue1' -AsUrl #-BackgroundColor Blue @caps
 
     if (Get-Module posh-git) {
-             # -Caps "&nf-pl-branch;", "`n" # nf-pl-branch requires a PowerLine font
         Show-PoshGitStatus -BeforeStatus '' -AfterStatus '' -PathStatusSeparator '' #-BackgroundColor Gray80 @caps
+            # -Caps "&nf-pl-branch;", "`n" # nf-pl-branch requires a PowerLine font
     }
 
         # You can use -Cap "`n" instead of a newline block to add a newline conditional on this block being output
     Show-LastExitCode -ForegroundColor 'VioletRed1' #@caps
 
+    # For right-aligned blocks, the first one is the furthest to the right
+
     # Use a short time format and a clock prefix (I don't need the EXACT time in my prompt)
-    # Because all of these are right-aligned, the first one is the furthest to the right
-    Show-Date -Format 'HH:mm' -Prefix '&twooclock;' -Foreground '#FFFFFF' -Alignment Right
+    Show-Date -Format 't' -Prefix '&twooclock;' -Foreground '#FFFFFF' -Alignment Right
 
     # Since this isn't right aligned, it starts a new line
     Show-HistoryId -Foreground 'White'
 
     # So the in-line prompt is just this one character:
-    New-TerminalBlock $PromptSymbol -Foreground 'Gray80' -Caps ''
+    New-TerminalBlock $PromptSymbol -Foreground 'Gray80' -AdminForeground Yellow -Caps ''
+
+    Exit-Prompt
 )
 
 function global:Prompt { -join $Prompt }
