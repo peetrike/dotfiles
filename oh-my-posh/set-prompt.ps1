847fbb12af8c07e029ae05b8e84ba752d02d02f0
Peter Wawa <meelisn@outlook.com>
2024-01-04 20:22:06 +0200

    remove redundant typecast

--- oh-my-posh/set-prompt.ps1
+++ oh-my-posh/set-prompt.ps1
@@ -1,5 +1,5 @@
 ﻿function Set-EnvVar {
-    $env:PSHistory = $($MyInvocation.HistoryId)
+    $env:PSHistory = $MyInvocation.HistoryId
 }
 Set-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVar' -Scope Global
 $env:POSH_THEMES_PATH = 'C:\Program Files (x86)\oh-my-posh\themes'
