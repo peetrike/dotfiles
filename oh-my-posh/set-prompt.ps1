31c472450a625246293563dbb9ee7160df90486a
Peter Wawa <meelisn@outlook.com>
2024-01-08 18:02:29 +0200

    add possibility to choose theme

--- oh-my-posh/set-prompt.ps1
+++ oh-my-posh/set-prompt.ps1
@@ -1,9 +1,18 @@
-﻿function Set-EnvVar {
+﻿[CmdletBinding()]
+param (
+        [string]
+    $Theme = 'pwtheme.omp.json'
+)
+function Set-EnvVar {
     $env:PSHistory = $MyInvocation.HistoryId
 }
 Set-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVar' -Scope Global
 $env:POSH_THEMES_PATH = 'C:\Program Files (x86)\oh-my-posh\themes'
 
+$ThemeFolder = Join-Path -Path (Split-Path -Path $profile) -ChildPath 'PoshThemes'
+if (-not $Theme.Contains('.')) {
+    $Theme += '.omp.json'
+}
     # https://ohmyposh.dev/docs/installation/customize
-$env:POSH_THEME = (Join-Path -Path (Split-Path -Path $profile) -ChildPath 'PoshThemes\pwtheme.omp.json')
+$env:POSH_THEME = (Join-Path -Path $ThemeFolder -ChildPath $Theme)
 oh-my-posh.exe init pwsh | Invoke-Expression
