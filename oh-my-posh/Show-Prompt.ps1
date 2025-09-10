2b7a02d041c1e0f234a8d890b920527345aa4902
Peter Wawa <meelisn@outlook.com>
2025-09-10 10:09:14 +0300

    use oh.my-posh print command to show prompt sample

--- oh-my-posh/Show-Prompt.ps1
+++ oh-my-posh/Show-Prompt.ps1
@@ -1,1 +1,9 @@
-﻿Get-PoshThemes -Path (Split-Path -Path $env:POSH_THEME -Parent)
+﻿[CmdletBinding()]
+param (
+    $Path = (Split-Path -Path $env:POSH_THEME -Parent)
+)
+
+Get-ChildItem -Path $Path -Filter *.omp.json | ForEach-Object {
+    Write-Output "`n" (Split-Path -Path $_ -Leaf).Replace('.omp.json', '')
+    oh-my-posh.exe print primary --config $_
+}
